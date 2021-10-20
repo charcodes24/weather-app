20f99236e31dd2456adf39ef46af8698b2c01f7a
charcodes24 <charcodes24@gmail.com>
2021-10-19 19:37:50 -0700

    Applied flexbox styling to components

--- src/ZipCode.js
+++ src/ZipCode.js
@@ -10,11 +10,19 @@ export default function ZipCode() {
     console.log(zipcode)
 
     return (
-        <div>
-            <form>
-                <input onChange={handleInput} type="text" name="zipcode" value={zipcode} placeholder="Enter a 5-digit zipcode..." />
-                <button type="submit">Get the weather!</button>
-            </form>
-        </div>
-    )
+      <div>
+        <form className="zipcode">
+          <input
+            onChange={handleInput}
+            type="text"
+            pattern="[0-9]"
+            name="zipcode"
+            value={zipcode}
+            maxlength="5"
+            placeholder="Enter a 5-digit zipcode..."
+          />
+          <button type="submit">Get the weather!</button>
+        </form>
+      </div>
+    );
 }
